13e17fd60a868a5428dd3ee0315031a876078dfe
Randall Mason <Randall@Mason.CH>
2024-12-03 14:03:43 +0000

    Collapse day template into dayX.rs

--- src/dayX.rs
+++ src/dayX.rs
@@ -17,12 +17,19 @@ pub fn part2(input: &i32) -> usize {
 mod tests {
     use super::{input_generator, part1, part2};
 
+    const SAMPLES: &[(&str, u32, u32)] = &[
+        (r#"multilineInputString"#, 4, 5)];
+
     #[test]
     fn sample1() {
-        assert_eq!(part1(&input_generator("")), 7);
+        for (sample, answer, _) in SAMPLES {
+            assert_eq!(part1(&input_generator(sample)), *answer);
+        }
    }
     #[test]
     fn sample2() {
-        assert_eq!(part2(&input_generator("")), 336);
+        for (sample, _, answer) in SAMPLES {
+            assert_eq!(part2(&input_generator(sample)), *answer);
+        }
    }
 }
